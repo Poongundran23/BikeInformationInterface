fbf743511b918c9fa8a8c1f5c62cb4ab91ad5a4e
Poongundran23 <poongundran27@gmail.com>
2021-07-11 17:42:25 +0530

    updated

---
+++ src/org/bike/Ktm.java
@@ -0,0 +1,22 @@
+package org.bike;
+
+public class Ktm implements Bike {
+
+	@Override
+	public void cost() {
+		System.out.println("KTM Cost: ");
+		
+	}
+
+	@Override
+	public void speed() {
+		System.out.println("KTM Speed: ");
+		
+	}
+	public static void main(String[] args) {
+		Ktm k = new Ktm();
+		k.cost();
+		k.speed();
+	}
+
+}
